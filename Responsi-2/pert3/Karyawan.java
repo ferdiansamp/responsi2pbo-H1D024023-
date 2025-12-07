877731a43639f1485b92f96f19ddc87bef490582
ferdiansamp <mohammad.samputra@mhs.unsoed.ac.id>
2025-12-07 13:32:04 +0700

    Initial clean commit

---
+++ Responsi-2/pert3/Karyawan.java
@@ -0,0 +1,13 @@
+class Karyawan {
+    // Deklarasikan variabel/state dasar (nama, gajiPokok)
+    // Gunakan access modifier yang tepat agar bisa diakses oleh subclass (misal: protected)
+    
+    
+    // Constructor untuk inisialisasi nama dan gajiPokok
+    
+    
+    // Method untuk menampilkan informasi dasar karyawan
+    void tampilInfo() {
+        
+    }
+}
---
+++ Responsi-2/pert3/UjiKaryawan.java
@@ -0,0 +1,22 @@
+class UjiKaryawan {
+    public static void main(String[] args) {
+        // Tampilkan Header
+        System.out.println("=== DATA KARYAWAN TECHMAJU ===");
+
+        // Buat objek Karyawan biasa (Budi)
+        
+        
+        // Tampilkan info Karyawan biasa
+        System.out.println("Status: Karyawan Biasa");
+        
+        
+        System.out.println(); // Baris baru/jarak
+
+        // Buat objek Manajer (Siti)
+        
+        
+        // Tampilkan info Manajer
+        System.out.println("Status: Manajer");
+        
+    }
+}
